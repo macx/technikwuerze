499f55dcd9fa55a0552cc5c7da6ea237566d537e
David Maciejewski <macx@macx.io>
2026-05-23 12:11:35 +0200

    refactor: site templates

--- site/templates/page.php
+++ site/templates/page.php
@@ -2,11 +2,15 @@
 
   <?php slot(); ?>
     <div class="page-header content narrow">
-      <h1 class="title"><?= $page->title()->html() ?></h1>
+      <h1 class="title"><?= $page->header()->or($page->title())->html() ?></h1>
+
+      <?php if ($page->lead()->isNotEmpty()): ?>
+        <p class="lead"><?= $page->lead()->kti() ?></p>
+      <?php endif; ?>
     </div>
 
     <div class="page-content content-text content narrow">
-      <?= $page->text()->toBlocks() ?>
+      <?= $page->blocks()->toBlocks() ?>
     </div>
   <?php endslot(); ?>
 
