4a390c569dd90e3f33d61020fea5ba87245cfe11
David Maciejewski <macx@macx.io>
2026-07-04 11:18:30 +0200

    feat: Add footnotes, cleanup styles

---
+++ site/snippets/footnotes_container.php
@@ -0,0 +1,7 @@
+<<?php echo option('sylvainjule.footnotes.wrapper'); ?> id="footnotes" class="footnotes-container">
+    <p class="text-eyebrow footnotes-heading">Fußnoten</p>
+
+    <ol class="footnotes-list">
+        <?php echo $footnotes; ?>
+    </ol>
+</<?php echo option('sylvainjule.footnotes.wrapper'); ?>>
---
+++ site/snippets/footnotes_entry.php
@@ -0,0 +1,14 @@
+<li id="fn-<?php echo $order ?>" value="<?php echo $order ?>">
+    <span class="footnote-entry">
+        <?php echo $note ?>
+        <?php if(option('sylvainjule.footnotes.back') && option('sylvainjule.footnotes.links')): ?>
+            <?php foreach($refs as $i => $refCount): ?>
+            <span class="footnotereverse">
+                <a href="#fnref-<?php echo $refCount ?>" title="<?php echo option('sylvainjule.footnotes.back.title', t('footnotes.back.title')) ?> <?php echo $order ?><?php echo count($refs) > 1 ? ' ' . chr(97 + $i) : '' ?>">
+                    <?php echo option('sylvainjule.footnotes.back') ?><?php echo count($refs) > 1 ? '&nbsp;' . chr(97 + $i) : '' ?>
+                </a>
+            </span>
+            <?php endforeach; ?>
+        <?php endif; ?>
+    </span>
+</li>
